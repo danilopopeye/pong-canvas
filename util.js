30e0d2089c9123a63d7696c1ae1ecf7983a31b14
Danilo Sousa <danilopopeye@gmail.com>
2011-11-25 19:01:42 -0200

    Added Function.bind: closes #1;

--- util.js
+++ util.js
@@ -20,3 +20,24 @@ if ( !window.requestAnimationFrame ) {
 	} )();
 
 }
+
+// Function.bind()
+if( ! ( 'bind' in Function.prototype ) ){
+  Function.prototype.bind = function(owner){
+    var that = this;
+
+    if( arguments.length <= 1 ){
+      return function(){
+        return that.apply( owner, arguments );
+      };
+    } else {
+      var args = Array.prototype.slice.call( arguments, 1 );
+
+      return function(){
+        return that.apply( owner, arguments.length === 0 ? args : args.concat(
+          Array.prototype.slice.call( arguments )
+        ));
+      };
+    }
+  };
+}
